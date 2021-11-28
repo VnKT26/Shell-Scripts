671c46630121772ef1fc4935bb89171b3c246838
Ventura Torres <34493084+VnKT26@users.noreply.github.com>
2021-11-28 14:46:01 -0700

    Update Prompt_User_Running_Low_on_Space_With_Display_5LargestFiles.sh

--- End User Maintenance Scripts/Prompt_User_Running_Low_on_Space_With_Display_5LargestFiles.sh
+++ End User Maintenance Scripts/Prompt_User_Running_Low_on_Space_With_Display_5LargestFiles.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
+#################################
+MIT License
 
+Copyright (c) 2021 Ventura Torres
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 #############################################################################
 #############################################################################
 #Author: Ventura Torres
@@ -39,7 +60,7 @@ Please make space on your computer by clearing out unnecesary files to ensure yo
 
 $largeFiles.
 
-Please contact IT if you have any questions or require any assistance. You will receive this prompt daily until you have at least 10 GB of free space on your mac." -button1 OK -defaultButton 1 -icon /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns #-iconSize pixels 300
+Please contact IT if you have any questions or require any assistance. You will receive this prompt daily until you have at least 15 GB of free space on your mac." -button1 OK -defaultButton 1 -icon /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns #-iconSize pixels 300
 
 fi
 
